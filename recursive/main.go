0bff8eb2b3bba115cf5300d51e05465a02a48104
Leandro Lima <llsilva@tutanota.com>
2022-06-17 22:08:32 -0300

    recursion higher value

--- recursive/main.go
+++ recursive/main.go
@@ -21,11 +21,27 @@ func countItens(arr []int) int {
 	} else {
 		return 1
 	}
+}
 
+func higherValue(arr []int) int {
+	arrReduced := arr[:len(arr)-1]
+	value := arr[len(arr)-1]
+	if len(arrReduced) >= 1 {
+		expectedToBeHigher := value
+		value := higherValue(arrReduced)
+		if value >= expectedToBeHigher {
+			return value
+		} else {
+			return expectedToBeHigher
+		}
+	} else {
+		return 0
+	}
 }
 
 func main() {
-	toSumAndCount := []int{1, 2, 3, 4, 5, 6}
+	toSumAndCount := []int{1, 2, 15, 3, 4, 5, 6}
 	fmt.Println("Sum:", soma(toSumAndCount))
 	fmt.Println("Count itens:", countItens(toSumAndCount))
+	fmt.Println("Higher value:", higherValue(toSumAndCount))
 }
